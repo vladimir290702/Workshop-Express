e938830c0a12fbf65c3dddab8f5e3eaabf782ce3
vladimir290702 <vladimirovichh@abv.bg>
2021-04-15 21:47:56 +0300

    add routes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
 const config = require('./config/config');
+const routes = require('./routes');
 const app = express();
 
 require('./config/express')(app);
 
-app.get('/', (req, res) => {
-    res.render('home', { layout: false });
-})
+app.use(routes);
+
 app.listen(config.PORT, () => {
     console.log(`Server is listening on port ${config.PORT}...`);
 })
